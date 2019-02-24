50dc0cd2b56e796fc0d2a4ea142a51c6d9d94f69
itsamanverma <amanvermame786@gmail.com>
2019-02-24 16:26:49 +0530

    PHP_rest_api started

---
+++ PHP_Rest_ApI/Config/Database.php
@@ -0,0 +1,14 @@
+<?php
+class Database
+{
+// DB params
+private $host ='localhost';
+private $db_name = 'Codeigniter';
+private $username = 'root';
+private $password = 'Root@123';
+private $conn;
+
+//DB conn
+
+}
+?>
